df80546a5cd76bec87812f55cb7aa404200826e8
JustARandomness <f.utin.s29@gmail.com>
2024-10-11 23:16:49 +0300

    Lab2 reworked

--- main.cpp
+++ main.cpp
@@ -1,54 +1,72 @@
 #include <opencv4/opencv2/opencv.hpp>
 #include <iostream>
+#include <vector>
 
-using namespace cv;
-
-uchar quantizePixel(uchar pixelValue, int n) {
-    int levels = 1 << n;
+int getClosestLevel(int pixel, int levels) {
     int step = 256 / levels;
-    return (pixelValue / step) * step;
+    return step * (pixel / step);
+};
+
+int checkIntOverflow(int value) {
+    if (value < 0) {
+        return 0;
+    }
+    if (value > 255) {
+        return 255;
+    }
+    return value;
 }
 
-int main(int argc, char** argv) {
-    Mat image = imread("../tosyapocalypsis.jpg", IMREAD_GRAYSCALE);
-    if (image.empty()) {
-        std::cout << "Could not open or find the image." << std::endl;
-        return -1;
-    }
+cv::Mat floydSteinberg(cv::Mat &image, int levels) {
+    cv::Mat outputImage = image.clone();
 
-    int num_colors = std::stoi(argv[1]);
-    if (num_colors < 1 || num_colors >= 8) {
-        std::cout << "num_colors must be between 1 and 7." << std::endl;
-        return -1;
-    }
-
-    int n = image.rows;
-    int m = image.cols;
-
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            uchar oldPixel = image.at<uchar>(i, j);
-            uchar newPixel = quantizePixel(oldPixel, num_colors);
-            image.at<uchar>(i, j) = newPixel;
-
-            int quantError = oldPixel - newPixel;
-
-            if (j + 1 < m) {
-                image.at<uchar>(i, j + 1) += quantError * 7 / 16;
+    for (int i = 0; i < image.rows; ++i) {
+         for (int j = 0; j < image.cols; ++j) {
+            int oldPixel = image.at<uchar>(i, j);
+            int newPixel = getClosestLevel(oldPixel, levels);
+            outputImage.at<uchar>(i, j) = newPixel;
+            int difference = oldPixel - newPixel;
+            if (j + 1 < image.cols) {
+                int pixel = outputImage.at<uchar>(i, j + 1);
+                outputImage.at<uchar>(i, j + 1) = checkIntOverflow(pixel + 7 * difference / 16);
+                if (i + 1 < image.rows) {
+                    int pixel = outputImage.at<uchar>(i + 1, j + 1);
+                    outputImage.at<uchar>(i + 1, j + 1) = checkIntOverflow(pixel + difference / 16);
+                }
             }
-            if (i + 1 < n) {
-                if (j > 0) {
-                    image.at<uchar>(i + 1, j - 1) += quantError * 3 / 16;
-                }
-                image.at<uchar>(i + 1, j) += quantError * 5 / 16;
-                if (j + 1 < m) {
-                    image.at<uchar>(i + 1, j + 1) += quantError * 1 / 16;
-                }
+            if (i + 1 < image.rows) {
+                int pixel = outputImage.at<uchar>(i + 1, j);
+                outputImage.at<uchar>(i + 1, j) = checkIntOverflow(pixel + 5 * difference / 16);
+            }
+            if (j - 1 >= 0 && i + 1 < image.rows) {
+                 int pixel = outputImage.at<uchar>(i + 1, j - 1);
+                outputImage.at<uchar>(i + 1, j - 1) = checkIntOverflow(pixel + 3 * difference / 16);
             }
         }
     }
 
-    imwrite("../outputImage.jpg", image);
+    return outputImage;
+}
 
+int main(int argc, char** argv) {
+    cv::Mat image = cv::imread("../tosyapocalypsis.jpg", cv::IMREAD_GRAYSCALE);
+
+    int n;
+    std::cin >> n;
+
+    if (n < 1 || n > 8) {
+        std::cout << "Invalid value" << std::endl;
+        return -1;
+    }
+
+    if (n == 8) {
+        imwrite("../outputImage.png", image);
+        return 0;
+    }
+
+    cv::Mat outputImage = floydSteinberg(image, n);
+
+    imwrite("../outputImage.png", outputImage);
+    
     return 0;
 }
