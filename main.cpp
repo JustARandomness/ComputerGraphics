ca7e0810f4253c0f508eaa07be640407a1c97dad
JustARandomness <f.utin.s29@gmail.com>
2024-11-19 16:19:41 +0300

    Possible to draw cubic bezier curve

--- main.cpp
+++ main.cpp
@@ -1,213 +1,7 @@
+#include <opencv4/opencv2/opencv.hpp>
 #include <iostream>
-#include "opencv4/opencv2/opencv.hpp"
 
 using namespace cv;
-
-enum CLPointType {LEFT, RIGHT, BEYOND, BEHIND, BETWEEN, ORIGIN, DESTINATION};
-enum IntersectType {SAME, PARALLEL, SKEW, SKEW_CROSS, SKEW_NO_CROSS};
-enum EType {TOUCHING, CROSS_LEFT, CROSS_RIGHT, INESSENTIAL};
-enum PType {INSIDE, OUTSIDE};
-enum fillType {EO, NZW};
-
-CLPointType classify(Point p1, Point p2, Point p) {
-    double ax = p2.x - p1.x;
-    double ay = p2.y - p1.y;
-    double bx = p.x - p1.x;
-    double by = p.y - p1.y;
-    double s = ax * by - bx * ay;
-    
-    if (s > 0) return LEFT;
-    if (s < 0) return RIGHT;
-    if ((ax * bx < 0) || (ay * by < 0)) return BEHIND;
-    if ((ax * ax + ay * ay) < (bx * bx + by * by)) return BEYOND;
-    if (p1.x == p.x && p1.y == p.y) return ORIGIN;
-    if (p2.x == p.x && p2.y == p.y) return DESTINATION;
-    return BETWEEN;
-}
-
-EType getEdgeType(Point o, Point d, Point a) {
-    switch(classify(o, d, a)) {
-        case LEFT:
-            if (a.y > o.y && a.y <= d.y) {
-                return CROSS_LEFT;
-            }
-            else {
-                return INESSENTIAL;
-            }
-        case RIGHT:
-            if (a.y > d.y && a.y <= o.y) {
-                return CROSS_RIGHT;
-            }
-            else {
-                return INESSENTIAL;
-            }
-        case BETWEEN:
-        case ORIGIN:
-        case DESTINATION:
-            return TOUCHING;
-        default:
-            return INESSENTIAL;
-    }
-}
-
-PType PInPolygonEOMode(Point p, const std::vector<Point>& points) {
-    int n = points.size();
-    int param = 0;
-    for (int i = 0; i < n; ++i) {
-        switch(getEdgeType(points[i], points[(i + 1) % n], p)) {
-            case TOUCHING:
-                return INSIDE;
-            case CROSS_LEFT:
-            case CROSS_RIGHT:
-                param = 1 - param;
-        }
-    }
-    if (param == 1) {
-        return INSIDE;
-    } 
-    else {
-        return OUTSIDE;
-    }
-}
-
-PType PInPolygonNZWMode(Point p, const std::vector<Point>& points) {
-    int n = points.size();
-    int param = 0;
-    for (int i = 0; i < n; ++i) {
-        switch(getEdgeType(points[i], points[(i + 1) % n], p)) {
-            case TOUCHING:
-                return INSIDE;
-            case CROSS_LEFT:
-                param++;
-                break;
-            case CROSS_RIGHT:
-                param--;
-                break;
-        }
-    }
-    
-    if (param == 0) {
-        return OUTSIDE;
-    } 
-    else {
-        return INSIDE;
-    }
-}
-
-void fillPolygon(Mat& img, std::vector<Point>& points, fillType type) {
-    int n = img.rows;
-    int m = img.cols;
-    if (type == EO) {
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                if (PInPolygonEOMode(Point(j, i), points) == INSIDE) {
-                    img.at<Vec3b>(i, j) = Vec3b(0, 0, 255);
-                }
-            }
-        }
-    }
-    else {
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                if (PInPolygonNZWMode(Point(j, i), points) == INSIDE) {
-                    img.at<Vec3b>(i, j) = Vec3b(0, 0, 255);
-                }
-            }
-        }
-    }
-}
-
-IntersectType intersect(Point a, Point b, Point c, Point d, double* t) {
-    double nx = d.y - c.y;
-    double ny = c.x - d.x;
-    CLPointType type;
-    double denom = nx * (b.x - a.x) + ny * (b.y - a.y);
-    if (denom == 0) {
-        type = classify(c, d, a);
-        if (type == LEFT || type == RIGHT) {
-            return PARALLEL;
-        }
-        else {
-            return SAME;
-        }
-    }
-    double num = nx * (a.x - c.x) + ny * (a.y - c.y);
-    *t = -num/denom;
-    return SKEW;
-}
-
-IntersectType cross(Point a, Point b, Point c, Point d, double* tab, double* tcd) {
-    IntersectType type = intersect(a, b, c, d, tab);
-    if (type == SAME || type == PARALLEL) {
-        return type;
-    }
-    if ((*tab < 0) || (*tab > 1)) {
-        return SKEW_NO_CROSS;
-    }
-    intersect(c, d, a, b, tcd);
-    if ((*tcd < 0) || (*tcd > 1)) {
-        return SKEW_NO_CROSS;
-    }
-     return SKEW_CROSS;
-}
-
-bool isComplex(const std::vector<Point>& points) {
-    int n = points.size();
-    if (n < 3) return true;
-    for (int i = 0; i < n; ++i) {
-        Point a = points[i];
-        Point b = points[(i + 1) % n];
-
-        for (int j = i + 2; j < n; ++j) {
-            if (j == (i + n - 1) % n) {
-                continue;
-            }
-
-            Point c = points[j];
-            Point d = points[(j + 1) % n];
-
-
-            double tab, tcd;
-            IntersectType type = cross(a, b, c, d, &tab, &tcd);
-            if (type == SKEW_CROSS) {
-                return true;
-            }
-        }
-    }
-    return false;
-}
-
-bool isConvex(const std::vector<Point>& points) {
-    int n = points.size();
-    if (n < 3) return false;
-
-    CLPointType initialSide = LEFT;
-    bool sideSet = false;
-
-    for (int i = 0; i < n; ++i) {
-        cv::Point p1 = points[i];
-        cv::Point p2 = points[(i + 1) % n];
-
-        
-        for (int j = 0; j < n; ++j) {
-            if (j == i || j == (i + 1) % n) continue;
-
-            CLPointType position = classify(p1, p2, points[j]);
-            
-            if (position == LEFT || position == RIGHT) {
-                if (!sideSet) {
-                    initialSide = position;
-                    sideSet = true;
-                } 
-                else if (position != initialSide) {
-                    return false;
-                }
-            }
-        }
-    }
-
-    return true;
-}
 
 void drawLine(Mat& img, Point p1, Point p2) {
     int x2 = p2.x, y2 = p2.y;
@@ -269,60 +63,30 @@ void drawLine(Mat& img, Point p1, Point p2) {
     }
 }
 
-void drawPolygon(Mat& img, std::vector<Point>& points) {
-    for (int i = 0; i < points.size(); ++i) {
-        drawLine(img, points[i], points[(i + 1) % points.size()]);
+Point getCubicBezierCurvePoint(Point p0, Point p1, Point p2, Point p3, double t) {
+    double B0 = (1 - t) * (1 - t) * (1 - t);
+    double B1 = 3 * t * (1 - t) * (1 - t);
+    double B2 = 3 * t * t * (1 - t);
+    double B3 = t * t * t;
+    int x = int(round(B0 * p0.x + B1 * p1.x + B2 * p2.x + B3 * p3.x));
+    int y = int(round(B0 * p0.y + B1 * p1.y + B2 * p2.y + B3 * p3.y));
+    return Point(x, y);
+}
+
+void drawCubicBezierCurve(Mat& img, Point p0, Point p1, Point p2, Point p3, int N) {
+    double step = 1.0 / (N - 1);
+    for (int i = 0; i < N; ++i) {
+        Point p = getCubicBezierCurvePoint(p0, p1, p2, p3, i * step);
+        Point q = getCubicBezierCurvePoint(p0, p1, p2, p3, (i + 1) * step);
+        drawLine(img, p, q);
     }
 }
 
-int main(int argc, char** argv) {
-    Mat img1(500, 500, CV_8UC3, Scalar(255, 255, 255));
-    Mat img2(500, 500, CV_8UC3, Scalar(255, 255, 255));
-
-    int n;
-    std::cout << "Enter number of points: ";
-    std::cin >> n;
-
-    if (n <= 0) {
-        std::cout << "Invalid number of points" << std::endl;
-        return -1;
-    }
-
-    std::vector<Point> points;
-    for (int i = 0; i < n; ++i) {
-        int x, y;
-        std::cout << "Enter point " << i + 1 << ": ";
-        std::cin >> x >> y;
-        points.push_back(Point(x, y));
-    }
-
-    drawPolygon(img1, points);
-    drawPolygon(img2, points);
-
-    if (isConvex(points)) {
-        std::cout << "Многоугольник выпуклый" << std::endl;
-    }
-    else {
-        std::cout << "Многоугольник вогнутый" << std::endl;
-    }
-
-    if (isComplex(points)) {
-        std::cout << "Многоугольник сложный" << std::endl;
-    }
-    else {
-        std::cout << "Многоугольник простой" << std::endl;
-    }
-
-    imwrite("../polygon.png", img1);
-
-    fillPolygon(img1, points, EO);
-    fillPolygon(img2, points, NZW);
-
-
-    imwrite("../fillEO.png", img1);
-    imwrite("../fillNZW.png", img2);
-    
+int main() {
+    Mat img(500, 500, CV_8UC3, Scalar(255, 255, 255));
+    drawCubicBezierCurve(img, Point(0, 0), Point(200, 20), Point(200, 50), Point(0, 100), 1000);
+    namedWindow("img");
+    imshow("img", img);
     waitKey(0);
- 
     return 0;
 }
